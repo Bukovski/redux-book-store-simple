7143f201434bbd325d016a0946e065893b8e784b
Bukovski <kmanankov@bk.ru>
2020-02-18 21:07:12 +0300

    add: style to loading indicator

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Bookstore React-Redux project
 
-I use context for get data to component
+I use context for get data to component,
+
+I took this animation for a loading indicator from the [codepen link](https://codepen.io/xhepigerta/pen/bprWbR)
--- src/components/indicators/loading-indicator.js
+++ src/components/indicators/loading-indicator.js
@@ -3,7 +3,11 @@ import './loading-indicator.css';
 
 
 const LoadingIndicator = () => {
-  return <div>loading...</div>;
+  return <div className="cssload-loader">
+    <div className="cssload-inner cssload-one" />
+    <div className="cssload-inner cssload-two" />
+    <div className="cssload-inner cssload-three" />
+  </div>;
 };
 
 
