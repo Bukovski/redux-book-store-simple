be885eb0fdc494219504f9c9b699a0b2c24664a4
Bukovski <kmanankov@bk.ru>
2020-02-24 12:29:11 +0300

    fix: header menu href

--- src/components/shop-header/shop-header.jsx
+++ src/components/shop-header/shop-header.jsx
@@ -7,11 +7,11 @@ const ShopHeader = ({ orderTotal, orderQuantity, handleClick }) => {
   return (
     <header
       className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <a className="header-shop__logo mr-md-auto" href="/#">Itbook store</a>
-      <a className="header-shop__cart" href="/#" onClick={ handleClick }>
+      <a className="header-shop__logo mr-md-auto" href="/">Itbook store</a>
+      <span className="header-shop__cart" onClick={ handleClick }>
         <i className="header-shop__cart-icon fa fa-shopping-cart" />
         { orderQuantity } items ${ orderTotal }
-      </a>
+      </span>
     </header>
   );
 };
