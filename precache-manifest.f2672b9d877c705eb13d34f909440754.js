547a0dea22af29f6bcf11a70a549e29a534819e7
Bukovski <kmanankov@bk.ru>
2020-02-24 12:12:07 +0300

    Updates

---
+++ precache-manifest.f2672b9d877c705eb13d34f909440754.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82f76291aceddc18e35a93c47750e18",
+    "url": "/redux-book-store-simple/index.html"
+  },
+  {
+    "revision": "7e478465797f2aac4374",
+    "url": "/redux-book-store-simple/static/css/2.76e375e2.chunk.css"
+  },
+  {
+    "revision": "69b729c93b3f0f241804",
+    "url": "/redux-book-store-simple/static/css/main.dd14d9b2.chunk.css"
+  },
+  {
+    "revision": "7e478465797f2aac4374",
+    "url": "/redux-book-store-simple/static/js/2.0e69625d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/redux-book-store-simple/static/js/2.0e69625d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b729c93b3f0f241804",
+    "url": "/redux-book-store-simple/static/js/main.b7d74216.chunk.js"
+  },
+  {
+    "revision": "33d48272b515d252a078",
+    "url": "/redux-book-store-simple/static/js/runtime-main.0001bf1b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/redux-book-store-simple/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/redux-book-store-simple/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/redux-book-store-simple/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/redux-book-store-simple/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/redux-book-store-simple/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
